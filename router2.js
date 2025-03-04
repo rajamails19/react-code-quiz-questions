d0b6aa1767b9523bef3de5617d99ec2729cbee8f
Raja V <rajamails19@gmail.com>
2025-03-04 11:47:04 -0500

    Create router2.js

---
+++ router2.js
@@ -0,0 +1,9 @@
+import { BrowserRouter as Router, Route, _____ } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <_____ path="/about" element={<About />} />
+    </Router>
+  );
+}
