fb989c6d50e0aedddc40f22f20337a26c2d122e8
Raja V <rajamails19@gmail.com>
2025-03-04 11:43:22 -0500

    Create router1.js

---
+++ router1.js
@@ -0,0 +1,16 @@
+import { ________, Routes, Route } from 'react-router-dom';
+
+function Navigation() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <________ to="/">Home</________>
+        </li>
+        <li>
+          <________ to="/about">About</________>
+        </li>
+      </ul>
+    </nav>
+  );
+}
